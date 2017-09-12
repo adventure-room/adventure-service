9904187f1df2f1c58b60d053dd42de345727abcd
Erik Mulder <ewjmulder@yahoo.com>
2017-09-12 13:58:49 +0200

    Rename of project and package + moved apis to other project

--- server/src/main/java/com/programyourhome/iotadventure/server/scripts/ScriptRunner.java
+++ server/src/main/java/com/programyourhome/iotadventure/server/scripts/ScriptRunner.java
@@ -1,5 +1,6 @@
 package com.programyourhome.iotadventure.server.scripts;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
@@ -7,12 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScriptRunner {
 
-	public void runScript(String name, Map<String, String> arguments) {
-		try {
-			Class.forName(name).getMethod("runFromJava", Map.class).invoke(null, arguments);
-		} catch (Exception e) {
-			throw new IllegalStateException("Exception while trying to run script " + name, e);
-		}
-	}
+    /**
+     * Main method to start a script directly, mainly for testing purposes.
+     */
+    public static void main(String[] args) {
+        if (args.length % 2 == 0) {
+            System.out.println("Usage: ScriptRunner <script-name> (<script-arg-name> <script-arg-value>)*");
+        }
+        Map<String, String> arguments = new HashMap<>();
+        for (int i = 1; i < args.length; i += 2) {
+            arguments.put(args[i], args[i + 1]);
+        }
+        new ScriptRunner().runScript(args[0], arguments);
+    }
+
+    public void runScript(String name, Map<String, String> arguments) {
+        try {
+            Class.forName(name).getMethod("runFromJava", Map.class).invoke(null, arguments);
+        } catch (Exception e) {
+            throw new IllegalStateException("Exception while trying to run script " + name, e);
+        }
+    }
 
 }
